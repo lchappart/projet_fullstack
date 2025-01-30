571a0627873d4ce9f7dfb60464effd41a2e97ded
Léo Chappart <leo.chappart@coda-student.school>
2025-01-30 17:41:35 +0100

    No more delete problems and README update

--- Assets/JS/components/users.js
+++ Assets/JS/components/users.js
@@ -7,9 +7,9 @@ export const fillTableUsers = (data, tableBody) => {
                     <td>${data[i].username}</td>
                     <td><a href="#"><i class="is-enabled-icon ${data[i].enabled === 1 ? 'fa-solid fa-check text-success' : 'fa-solid fa-xmark text-danger'}" data-id="${data[i].id}"></i></a></td>
                     <td>
-                         <button type="button" class="btn btn-primary delete-btn" data-id="${data[i].id}">
-                              <i class="fa-solid fa-trash text-danger delete-btn"></i>
-                            </button>
+                         <a href="#" style="text-decoration: none" class="delete-btn">
+                            <i class="fa-solid fa-trash text-danger" data-id="${data[i].id}"></i>
+                        </a>
                         <a href="index.php?component=user&action=edit&id=${data[i].id}">
                             <i class="fa-solid fa-pen ms-3"></i>
                         </a>
